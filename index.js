de62ba69733955a54d061e81354223cf5cf25c22
the-coding-catalyst <thecodingcatlyst@gmail.com>
2023-03-09 13:27:02 +0530

    wr

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const port = 5000;
+const port = 5001;
 const url = 'https://api.telegram.org/bot';
 const apiToken = process.env.telegramToken;
 // Configurations
 app.use(bodyParser.json());
 // Endpoints
 app.post('/', (req, res) => {
-     // console.log(req.body);
+     
      const chatId = req.body.message.chat.id;
      const sentMessage = req.body.message.text;
      // Regex for hello
+     console.log(req.body);
      
-     if (sentMessage) {
-        console.log("condition changed")
+     if (sentMessage.match(/hello/gi)) {
+        console.log("----------")
           axios.post(`${url}${apiToken}/sendMessage`,
                {
                     chat_id: chatId,
