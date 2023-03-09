e979a49a251a79c5526271c8ba71e76ac61ea873
the-coding-catalyst <thecodingcatlyst@gmail.com>
2023-03-09 14:53:02 +0530

    subscription

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const port = 5001;
 const url = 'https://api.telegram.org/bot';
 const apiToken = '5602715484:AAFoeGmUud57haPHYvfKdV8MYvBEmbv88zQ'
+const subscribers = []
 // Configurations
 app.use(bodyParser.json());
 // Endpoints
@@ -14,21 +15,36 @@ app.post('/', (req, res) => {
      const sentMessage = req.body.message.text;
      // Regex for hello
      
-     if (sentMessage.match(/hello/gi)) {
-        console.log("here1")
+     if (sentMessage.match(/subscribe/gi)) {
+        
+        // console.log("here1")
           axios.post(`${url}${apiToken}/sendMessage`,
                {
                     chat_id: chatId,
-                    text: 'hello back'
+                    text: 'You have subscribed for daily updates'
                })
                .then((response) => { 
                     res.status(200).send(response);
-                    console.log("here2", response)
+                    // console.log("here2", response)
                }).catch((error) => {
-                    console.log("here3",error)
+                    // console.log("here3",error)
                     res.send(error);
                });
-     } else {
+     }else if (sentMessage.match(/unsubscribe/gi){
+        axios.post(`${url}${apiToken}/sendMessage`,
+               {
+                    chat_id: chatId,
+                    text: 'You have been unsubscribed'
+               })
+               .then((response) => { 
+                    res.status(200).send(response);
+                    // console.log("here2", response)
+               }).catch((error) => {
+                    // console.log("here3",error)
+                    res.send(error);
+               });
+     }
+      else {
           // if no hello present, just respond with 200 
           res.status(200).send({});
      }
